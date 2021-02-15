0b8033e23956f1b57a09d91615b58b2ee53c9595
Eliot Cowley <eliot93@live.com>
2021-02-15 15:38:37 -0800

    Added human, if you are close it targets him (#6)

---
+++ BrackeysGameJam2021/Assets/Scripts/Human.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    private GameObject target;
+
+    private void Start()
+    {
+        this.target = this.transform.GetChild(0).gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(Constants.Tag_GerbilMain))
+        {
+            this.target.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag(Constants.Tag_GerbilMain))
+        {
+            this.target.SetActive(false);
+        }
+    }
+}
