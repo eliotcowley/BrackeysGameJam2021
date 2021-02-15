b1badae579edb1163a54871f9f4adf55d67a9628
Eliot Cowley <eliot93@live.com>
2021-02-15 11:48:20 -0800

    If gerbil follower touches trap, it dies (#4)

---
+++ BrackeysGameJam2021/Assets/Scripts/Trap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gerbilDieFXPrefab;
+
+    [SerializeField]
+    private float gerbilDieFXDestroyDelay = 1f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(Constants.Tag_GerbilFollower))
+        {
+            GameObject gerbilDieFX = Instantiate(this.gerbilDieFXPrefab, other.attachedRigidbody.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            GerbilMain.Instance.SwarmCount--;
+            Destroy(gerbilDieFX, this.gerbilDieFXDestroyDelay);
+        }
+    }
+}
